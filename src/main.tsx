8fe77859f10435079d430c914c8143c90cf8a713
Daria Parashchenko <demiiquartz@gmail.com>
2025-12-16 17:30:11 +0100

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App";
+import Cards from "./components/Cards";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/cards" element={<Cards />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
